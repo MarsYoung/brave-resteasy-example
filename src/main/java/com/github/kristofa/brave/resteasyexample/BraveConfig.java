96f9ca4cc76fe7a02187ddb8ebdd6934b30b370c
马志宇(无线运营中心) <zhiyuma@sohu-inc.com>
2016-04-06 19:59:47 +0800

    change server

--- src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
+++ src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
@@ -11,6 +11,7 @@ import com.github.kristofa.brave.http.StringServiceNameProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import com.github.kristofa.brave.scribe.ScribeSpanCollector;
 
 import java.util.Arrays;
 
@@ -22,8 +23,8 @@ public class BraveConfig {
     public Brave brave() {
        Brave.Builder builder = new Brave.Builder();
 //       return builder.spanCollector(new KafkaSpanCollector("192.168.99.100:9092"))
-//       return builder.spanCollector(new ScribeSpanCollector("192.168.99.100", 9410))
-       return builder.spanCollector(new LoggingSpanCollector())
+       return builder.spanCollector(new ScribeSpanCollector("10.16.12.137", 9410))
+               //      return builder.spanCollector(new LoggingSpanCollector())
                .traceFilters(Arrays.<TraceFilter>asList(new FixedSampleRateTraceFilter(1)))
                .build();
     }
@@ -31,7 +32,7 @@ public class BraveConfig {
     @Bean
     @Scope(value = "singleton")
     public ServiceNameProvider serviceNameProvider() {
-        return new StringServiceNameProvider("test_service");
+        return new StringServiceNameProvider("brave");
     }
 
     @Bean
